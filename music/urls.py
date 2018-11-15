b38cd374b4e426f87d3e626aaa1251ff1768ecb4
Dave Old Computer <daveperry10@gmail.com>
2018-11-14 22:14:56 -0800

    end of tutorial 33.  image problem.

--- music/urls.py
+++ music/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 from django.conf.urls import url, include   #older
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'music'
 
@@ -18,7 +20,14 @@ urlpatterns = [
 
     # /music/album/add/
     url(r'album/add/$', views.AlbumCreate.as_view(), name='album-add'),
+
+    # /music/album/2/
+    url(r'album/(?P<pk>[0-9]+)/$', views.AlbumUpdate.as_view(), name='album-update'),
+
+    # /music/album/2/delete/
+    url(r'album/(?P<pk>[0-9]+)/delete/$', views.AlbumDelete.as_view(), name='album-delete'),
 ]
 
-# url(r'^$', views.index, name='index'),
-# url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- music/views.py
+++ music/views.py
@@ -6,6 +6,7 @@ Generic Class Templates
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Album
+from django.urls import reverse_lazy
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
@@ -21,3 +22,12 @@ class DetailView(generic.DetailView):
 class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumDelete(DeleteView):
+     model=Album
+     success_url = reverse_lazy('music:index')
+
